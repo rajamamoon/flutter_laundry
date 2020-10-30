8c4d215b49844ccd4e377db611898c91ba9b4566
Mamoon Raja <59180384+rmamoonx@users.noreply.github.com>
2020-10-30 22:05:36 +0400

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,7 @@ class Home extends StatelessWidget {
             clipper: ClippingClass(),
             child: Container(
               width: double.infinity,
-              height: MediaQuery.of(context).size.height*4/7,
+              height: MediaQuery.of(context).size.height * 4 / 7,
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   begin: Alignment.topCenter,
@@ -39,20 +39,17 @@ class Home extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                Text("Hi John",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                    )),
                 Text(
-                  "Hi John",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32,
-                  )
-                ),
-                Text(
-                  "Get your laundry washed, folded \nand delivered straight to your door.",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  )
-                ),
+                    "Get your laundry washed, folded \nand delivered straight to your door.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    )),
               ],
             ),
           ),
@@ -63,39 +60,38 @@ class Home extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 GestureDetector(
-                  onTap:(){ Navigator.push(context, MaterialPageRoute(builder: (context) => Detailes()));},
-                                  child: Row(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Detailes()));
+                  },
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                                            _customCard(
+                      _customCard(
                           imageUrl: "washing-machine.png",
                           item: "Wash",
-                          duration: "1 Day"
-                        ),
-                        _customCard(
-                          imageUrl: "dry.png",
-                          item: "Dry",
-                          duration: "3 Days"
-                        )
+                          duration: "1 Day"),
+                      _customCard(
+                          imageUrl: "dry.png", item: "Dry", duration: "3 Days")
                     ],
                   ),
                 ),
-                SizedBox(height: 40,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      _customCard(
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _customCard(
                         imageUrl: "clean.png",
                         item: "Premium",
-                        duration: "3 Days"
-                      ),
-                      _customCard(
+                        duration: "3 Days"),
+                    _customCard(
                         imageUrl: "shoe.png",
                         item: "Others",
-                        duration: "3 Days"
-                      )
-                    ],
-                  ),
+                        duration: "3 Days")
+                  ],
+                ),
               ],
             ),
           )
@@ -104,51 +100,45 @@ class Home extends StatelessWidget {
     );
   }
 
-  _customCard({String imageUrl, String item, String duration}){
+  _customCard({String imageUrl, String item, String duration}) {
     return SizedBox(
-        height: 168,
-        width: 150,
-        child: Card(
-    color: Colors.white,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(20)
-    ),
-    elevation: 10,
-    child: Padding(
-      padding: EdgeInsets.all(8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset("assets/home_images/" + imageUrl),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                                    item,
-                                    style: TextStyle(fontSize: 24),
-                                  ),
-                                  Text(duration)
-              ],
-            ),
-          )
-        ],
-      ),
-    ),
+      height: 168,
+      width: 150,
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        elevation: 10,
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("assets/home_images/" + imageUrl),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      item,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    Text(duration)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
-
-
-
 }
 
-class ClippingClass extends CustomClipper<Path>{
+class ClippingClass extends CustomClipper<Path> {
   @override
-
-    Path getClip(Size size) {
+  Path getClip(Size size) {
     var path = Path();
     path.lineTo(0.0, size.height);
     var controlPoint = Offset(size.width - (size.width / 2), size.height - 120);
