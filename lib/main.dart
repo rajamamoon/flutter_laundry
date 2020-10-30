a7f60b3b66e1570a9d051e76406fa59cc4ded05c
Mamoon Raja <59180384+rmamoonx@users.noreply.github.com>
2020-10-30 21:42:10 +0400

    New Template

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'details.dart';
+import 'home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
