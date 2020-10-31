da51e965946555c7362a0aa9d32883a645ffe54a
Mamoon Raja <59180384+rmamoonx@users.noreply.github.com>
2020-10-31 15:07:51 +0400

    New Template

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:laundry_app/Screens/home.dart';
+import 'Constant/Constant.dart';
+import 'Screens/ProfilePage.dart';
+import 'Screens/SplashScreen.dart';
+import 'Screens/signup.dart';
+import 'Screens/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Laundry App',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+          primaryColor: Color(0xff0082CD), primaryColorDark: Color(0xff0082CD)),
+      home: new SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        PROFILE: (BuildContext context) => new LoginPage(),
+        '/signup': (BuildContext context) => new SignupPage(),
+        '/home': (BuildContext context) => new Home(),
+        '/profile': (BuildContext context) => new ProfilePage(),
+      },
+    );
+  }
+}
